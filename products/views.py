64d88b8b1799602dd161e3b94f8b10dcf4a77f9f
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-19 18:42:56 +0100

    update security settings

--- products/views.py
+++ products/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting """
+
     products = Product.objects.all()
+    categories = None
+    
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET.get('category').split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            
     context = {
-        'products': products
+        'products': products,
+        'current_categories': categories,
     }
-
     return render(request, 'products/products.html', context)
-
+   
 def product_detail(request, product_id):
     """ A view to show a single product's details """
     product = get_object_or_404(Product, pk=product_id)
     context = {
-        'product': product
+        'product': product,
+    
     }
 
     return render(request, 'products/product_detail.html', context)
