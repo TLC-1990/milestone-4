9a328073797fe5121e6d75f6e768a3f5b2e41afd
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-23 17:06:55 +0100

    add placeholder view for profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,7 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from products.models import Order
-from custom_artwork_request.models import CustomArtworkRequest
+from django.contrib.auth.decorators import login_required
 
 @login_required
 def profile(request):
-    orders = Order.objects.filter(user=request.user)
-    custom_requests = CustomArtworkRequest.objects.filter(user=request.user)
-    return render(request, 'profiles/profile.html', {
-        'orders': orders,
-        'custom_requests': custom_requests,
-    })
+    # Placeholder: add logic to fetch user profile and requests later
+    return render(request, 'profiles/profile.html')
