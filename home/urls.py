b8255a063314b2b1258421723bd879f45d3a060d
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-23 12:16:29 +0100

    add artist biography template

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('biography/', views.artist_biography, name='artist_biography'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    """ A view to return the index page """
-    
+    """A view to return the index page."""
     return render(request, 'home/index.html')
+
+def artist_biography(request):
+    """A view to return the artist biography page."""
+    return render(request, 'home/artist_biography.html')
