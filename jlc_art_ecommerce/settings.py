afc99753539aedfee7ca0d65e6e253eaf8ca7c43
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-23 14:38:17 +0100

    fix navbar function so custom artwork request and artist biography are sent to the collapsed navmenu on smaller screens

--- jlc_art_ecommerce/settings.py
+++ jlc_art_ecommerce/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth',),
+            os.path.join(BASE_DIR, 'templates', 'allauth', 'account',),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
