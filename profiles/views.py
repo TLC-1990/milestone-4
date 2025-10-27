78dc84db981acda1b8a4ad95f57b01183c9074b5
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-27 16:58:39 +0000

    pull custom artwork requests from database to profile model. Ensure choices are shown with friendly names using get_FOO_display()

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from custom_artwork_request.models import CustomArtworkRequest
 
 @login_required
 def profiles(request):
-    return render(request, 'profiles/profile.html')
+    user_requests = CustomArtworkRequest.objects.filter(user=request.user)
+    context = {
+        'user_requests': user_requests,
+    }
+    return render(request, 'profiles/profile.html', context)
