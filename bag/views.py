9bca11e05e1258ac93f2fc0473fbaf593df12e44
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-28 21:32:46 +0000

    add add_to_bag function for product detail pages

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def view_bag(request):
     """A view to return the shopping bag page."""
     
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """A view to add a specified product to the shopping bag."""
+
+    redirect_url = request.POST.get('redirect_url')
+
+    bag = request.session.get('bag', {})
+    bag[item_id] = bag.get(item_id, 0) + 1
+    request.session['bag'] = bag
+
+    return redirect(redirect_url)
--- jlc_art_ecommerce/settings.py
+++ jlc_art_ecommerce/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -146,3 +147,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STANDARD_DELIVERY_PERCENTAGE = 25
