cae17cfc401a293d11b924ce2f2144175a0d032c
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-30 16:34:49 +0000

    add bag functionality, so user can add product from store to bag

--- bag/views.py
+++ bag/views.py
@@ -13,5 +13,5 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
     bag[item_id] = bag.get(item_id, 0) + 1
     request.session['bag'] = bag
-
+    print(request.session['bag'])
     return redirect(redirect_url)
