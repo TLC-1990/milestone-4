1bfb670f37e1bf8ed7a839a13b1ac235837127f5
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-10-30 20:29:30 +0000

    interim commit to update heroku

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from django.conf import settings
-from django.shortcuts import get_object_or_404
+#from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def bag_contents(request):
@@ -11,9 +11,9 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
     
-    for item_id in bag:
+    for item_id in list(bag):
         try:
-            product = get_object_or_404(Product, pk=item_id)
+            product = Product.objects.get(pk=item_id)
             total += product.price
             product_count += 1
             bag_items.append({
